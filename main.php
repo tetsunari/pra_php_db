53790f254cd185f9ca960bbcca3fa8cfc7fe8e33
tsunari <mtst.tty556@gmail.com>
2021-05-02 00:23:11 +0900

    error message

--- main.php
+++ main.php
@@ -1,14 +1,19 @@
 <?php
 
-$pdo = new PDO(
-  'mysql:host=db;dbname=myapp;charset=utf8mb4',
-  'dbuser',
-  'dbpass',
-  [
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  ]
-);
+try {
+	$pdo = new PDO(
+		'mysql:host=db;dbname=myapp;charset=utf8mb4',
+		'dbuser',
+		'dbpass',
+		[
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		]
+	);
 
-$stmt = $pdo->query("SELECT 5 + 3");
-$result = $stmt->fetch();
-var_dump($result);
+	$stmt = $pdo->query("SELCT 5 + 3");
+	var_dump($stmt->fetch());
+} catch (PDOException $e) {
+	echo $e->getMessage() . PHP_EOL;
+	exit;
+}
